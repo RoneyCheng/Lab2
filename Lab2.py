9995396522055e2208f1e949c0ea43137ffeda5a
RoneyCheng <WAILIN.24@ichat.sp.edu.sg>
2025-10-27 23:13:01 +0800

    A file that is used to calculate the BMI

---
+++ Lab2.py
@@ -0,0 +1,37 @@
+my_list = []  # global variable
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5,67,32)")
+
+def get_user_input():
+    inputList = input()
+    string_list = inputList.split(",")
+    float_list = []
+    for x in string_list:
+        float_list.append(float(x))
+    return float_list
+
+def calc_average_temperature(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    average = total / len(numbers)
+    print("The average is " + str(average))
+    return average
+
+def find_min_max(numbers):
+    print("The minimum temperature is " + str(min(numbers)))
+    print("The maximum temperature is " + str(max(numbers)))
+    return [min(numbers), max(numbers)]
+
+def sort_temperature(numbers):
+    sorted_list = sorted(numbers)
+    print("Sorted temperatures: " + str(sorted_list))
+    return sorted_list
+
+
+display_main_menu()
+my_list = get_user_input()
+calc_average_temperature(my_list)
+find_min_max(my_list)
+sort_temperature(my_list)
---
+++ bmi.py
@@ -0,0 +1,14 @@
+def calculate_bmi(height, weight):
+    print("Height = "+str(height))
+    print("Weight = "+str(weight))
+    bmi = weight/(height * height)
+    print("BMI = "+str(bmi))
+    if(bmi < 18.5): 
+        print("Under Weight ")
+    elif(18.5<= bmi and bmi<=25.0):
+        print("Normal Weight")
+    else:
+        print("Over Weight")
+
+
+calculate_bmi(height=1.73, weight=57)
